37ac74646cb8799bd349736e9d9afb1ab4f64005
Riska K A <alternative.xen@gmail.com>
2018-07-02 07:16:24 +0000

    add whitenoise

--- project/urls.py
+++ project/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from welcome.views import index, health
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
+    path('health/', health)
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path(r'__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
